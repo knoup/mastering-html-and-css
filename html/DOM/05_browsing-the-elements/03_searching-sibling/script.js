949f8c4291208ab9afcf5fc40b3d9ecaa14d6e02
Pedro Manoel <dev.pedromanoel@pm.me>
2021-02-19 10:51:31 -0300

    searching sibling

---
+++ html/DOM/05_browsing-the-elements/03_searching-sibling/script.js
@@ -0,0 +1,11 @@
+// Navegando pelos elementos
+
+const el = document.querySelector('header')
+// nextSibling nextElementSibling
+console.log(el.nextSibling)
+console.log(el.nextElementSibling)
+
+// previousSibling previousElementSibling
+console.log(el.previousSibling)
+console.log(el.previousElementSibling)
+
