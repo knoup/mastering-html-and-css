e8c7004ca0218c30356a9674cf9d968ca51cbdff
Pedro Manoel <dev.pedromanoel@pm.me>
2021-02-19 10:46:04 -0300

    child elements

---
+++ html/DOM/05_browsing-the-elements/02_taking-childElements/script.js
@@ -0,0 +1,12 @@
+// Navegando pelos elementos
+// childNodes e children
+const el = document.querySelector('body')
+
+console.log(el.children)
+
+// firstChild firstElementChild
+console.log(el.firstChild)
+console.log(el.firstElementChild)
+// lastChild lastElementChild
+console.log(el.lastChild)
+console.log(el.lastElementChild)
